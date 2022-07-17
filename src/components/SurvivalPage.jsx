d2cb57d6a7a56d98db7000551804738cc5352a32
tekyuma <lawrencedaviid@gmail.com>
2022-07-17 23:27:23 +0100

    render survival page

--- src/components/SurvivalPage.jsx
+++ src/components/SurvivalPage.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 const SurvivalPage = () => {
-  return <div className="survival-page">This is the survival Page</div>;
+  return (
+    <div className="survival-page">
+      <nav>
+        <button>back</button>
+        <p>memorygame</p>
+        <div className="nav-icons">
+          <div>hi</div>
+          <div>hi</div>
+        </div>
+      </nav>
+      <main>
+        <div className="scoreboard">
+          <div className="current-score">Current Score: 0</div>
+          <div className="high-score">High Score: 11</div>
+        </div>
+        <div className="cards">
+          <div className="card">card1</div>
+          <div className="card">card2</div>
+          <div className="card">card3</div>
+          <div className="card">card4</div>
+          <div className="card">card5</div>
+          <div className="card">card6</div>
+          <div className="card">card7</div>
+          <div className="card">card8</div>
+          <div className="card">card9</div>
+          <div className="card">card10</div>
+          <div className="card">card11</div>
+          <div className="card">card12</div>
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default SurvivalPage;
